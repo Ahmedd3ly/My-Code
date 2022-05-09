ed8c9e3116ccb1e2cdb53f9f6858c17d4db50026
Ahmedd3ly <854260@swansea.ac.uk>
2022-05-09 01:28:09 +0100

    Minor edits in car park simulation

--- Java-Code/CarPark/src/Application.java
+++ Java-Code/CarPark/src/Application.java
@@ -2,7 +2,7 @@
 public class Application {
 	 public static void main(String[] args) throws InterruptedException {
 	        String name = "ParKing";
-	        int capacity = 20;
+	        int capacity = 10;
 	        Parking parking = new Parking(name, capacity);
 	        parking.start();
 	        Thread.sleep(60000);
